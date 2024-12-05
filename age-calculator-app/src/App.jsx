ecf3dbc004619b0c96428e70645feaf823251723
Paulo Carvalho <alopaulocarvalho@gmail.com>
2024-12-05 19:21:46 -0300

    Age calculator setup

--- age-calculator-app/src/App.jsx
+++ age-calculator-app/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="bg-white rounded-xl">
+        <div>
+          <form className="flex gap-4 p-8">
+
+            <div className='flex flex-col gap-1'>
+              <label htmlFor="day">DAY</label>
+              <input id="day" type="text" name='day' placeholder="DD"/>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <label htmlFor="month">MONTH</label>
+              <input id="month" type="text" name='month' placeholder="MM"/>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <label htmlFor="year">YEAR</label>
+              <input id="year" type="text" name='year' placeholder="YYYY"/>
+            </div>
+
+          </form>
+        </div>
+      </main>
     </>
   )
 }
